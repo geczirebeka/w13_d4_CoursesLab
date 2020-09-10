89e70f12d125d5631ccba2377974468cde55df5b
Rebeka Geczi <geczirebeka@gmail.com>
2020-09-10 11:48:34 +0100

    controllers setup with getall and getbyid

---
+++ src/main/java/com/codeclan/example/Courses/controllers/CourseController.java
@@ -0,0 +1,30 @@
+package com.codeclan.example.Courses.controllers;
+
+import com.codeclan.example.Courses.models.Course;
+import com.codeclan.example.Courses.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping(value= "/courses")
+    public ResponseEntity<List<Course>> getAllCourses() {
+        return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value= "/courses/{id}")
+    public ResponseEntity<Optional<Course>> getCourse(@PathVariable Long id) {
+        return new ResponseEntity<>(courseRepository.findById(id), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/codeclan/example/Courses/controllers/CustomerController.java
@@ -0,0 +1,30 @@
+package com.codeclan.example.Courses.controllers;
+
+import com.codeclan.example.Courses.models.Customer;
+import com.codeclan.example.Courses.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value= "/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value= "/customers/{id}")
+    public ResponseEntity<Optional<Customer>> getCustomer(@PathVariable Long id) {
+        return new ResponseEntity<>(customerRepository.findById(id), HttpStatus.OK);
+    }
+}
