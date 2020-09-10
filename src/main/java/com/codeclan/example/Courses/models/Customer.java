f7816759afa029aafef193647ecc4c97c7bb27e3
Rebeka Geczi <geczirebeka@gmail.com>
2020-09-10 10:45:32 +0100

    customer class setup

---
+++ src/main/java/com/codeclan/example/Courses/models/Customer.java
@@ -0,0 +1,83 @@
+package com.codeclan.example.Courses.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name= "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name= "name")
+    private String name;
+
+    @Column(name= "town")
+    private String town;
+
+    @Column(name= "age")
+    private int age;
+
+    @OneToMany(mappedBy="customer")
+    @JsonIgnoreProperties({"customer"})
+    private List<Booking> customerBookings;
+
+    public Customer(String name, String town, int age) {
+        this.name = name;
+        this.town = town;
+        this.age = age;
+        this.customerBookings = new ArrayList<>();
+    }
+
+    public Customer() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Booking> getCustomerBookings() {
+        return customerBookings;
+    }
+
+    public void setCustomerBookings(List<Booking> customerBookings) {
+        this.customerBookings = customerBookings;
+    }
+
+    public void addBooking(Booking booking) {
+        this.customerBookings.add(booking);
+    }
+}
