ccf7511dabf14f5da41be399d42c9f0427c40aa2
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-09-10 11:08:23 +0100

    customer and course relationships set up in booking model

--- src/main/java/com/codeclan/example/Courses/models/Booking.java
+++ src/main/java/com/codeclan/example/Courses/models/Booking.java
@@ -1,5 +1,7 @@
 package com.codeclan.example.Courses.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 @Entity
@@ -13,8 +15,20 @@ public class Booking {
     @Column(name= "date")
     private String date;
 
-    public Booking(String date) {
+    @JsonIgnoreProperties({"customerBookings"})
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @JsonIgnoreProperties({"courseBookings"})
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    public Booking(String date, Customer customer, Course course) {
         this.date = date;
+        this.customer = customer;
+        this.course = course;
     }
 
     public Booking() {
@@ -35,5 +49,21 @@ public class Booking {
 
     public void setDate(String date) {
         this.date = date;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
     }
 }
